abcf6f7604d280b09d6b9b5375f1efb40b2ad8f4
Rebecca Marks <marksre@umich.edu>
2018-03-20 18:53:26 -0400

    Revised python script creating bet qc page

--- make_bet_slices_ds008.py
+++ make_bet_slices_ds008.py
@@ -9,29 +9,18 @@ template_file = open("/home/marksre/Projects/local/share/templates/html/bet_slic
 # Open output file
 output_file = open("/home/marksre/Projects/output/html/sub002_bet_slices.html", "w")
 
-# Find lines that contain Subject ID variable
-for line in template_file:
-	line.find("{:03d}")
-	print line.format(subj_id)
-template_file = "\n".join(template_file)
+#Create list of lines from template file
+template_text = template_file.readlines()
+template_file.close()
 
 # Print template file but with subject ID
-print(template_file.format(subj_id))
 
+for line in template_text:
+    output_file.write(line.format(subj_id))
+output_file.close()
 
-output_file.write("test")
-output_file.write(print(template_file.format(subj_id)))
-
-# Save contents of print function to output
-with open("/home/marksre/Projects/local/share/templates/html/bet_slices.html", "r") as template:
-	with open("/home/marksre/Projects/output/html/sub002_bet_slices.html", "w") as output:
-		for line in template:
-		        line.find("{:03d}")
-       		print line.format(subj_id)
-			output.write(line)
-
-#output = "\n".join(template)
-
-#output_contents = str(template_file.format(subj_id))
-#output.write(output_contents)
-#output.close()
+#Another option:
+#with open("/home/marksre/Projects/local/share/templates/html/bet_slices.html", "r") as template:
+#    for line in template:
+#        output_file.write(line.format(17))
+#output_file.close()
